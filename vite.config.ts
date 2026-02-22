6227e1beecd6046a234330251dbe0c38f6ecb248
Sumeet Padavala <supadava@cisco.com>
2026-02-21 23:59:31 -0500

    Switch to experimental rolldown bundler

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         enabled: true,
       },
     }),
-    nitro({ preset: "bun" }), 
+    // nitro({ preset: "bun" }), 
     viteReact(),
     tailwindcss(),
   ],
