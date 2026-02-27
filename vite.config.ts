e3f4ec37ed1d6298a970e4d7e628dfbb083f5f80
Sumeet Padavala <sumeet.padavala@gmail.com>
2026-02-27 13:56:22 -0500

    Version bump, update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,31 @@
-import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
-import viteReact from "@vitejs/plugin-react";
-import {nitro} from "nitro/vite";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import contentCollections from '@content-collections/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
   plugins: [
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-    tanstackStart({
-      // FIXME spa prerender can't be disabled, and prerender currently fails quite often due to port conflicts (spams thousands of ports). Need to fix it and/or report the issue.
-      spa: {
-         enabled: true,
-     },
-      prerender: {
-        enabled: false
-      }
-    }),
-    viteReact(),
+    devtools(),
+    nitro({  }),
+    // contentCollections(),
+    tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
-    nitro()
+    tanstackStart({
+      spa: {
+        enabled: true,
+      },
+    }),
+    viteReact({
+      // babel: {
+      //   plugins: ['babel-plugin-react-compiler'],
+      // },
+    }),
   ],
-});
+})
+export default config
